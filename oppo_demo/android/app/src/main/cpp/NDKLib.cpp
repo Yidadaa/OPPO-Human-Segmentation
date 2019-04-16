568e987f239e41d24168d7b27fd82ef0a457b63a
Yidadaa <yidadaa@qq.com>
2019-04-16 22:20:37 +0800

    del(files): 删除无用文件

--- oppo_demo/android/app/src/main/cpp/NDKLib.cpp
+++ oppo_demo/android/app/src/main/cpp/NDKLib.cpp
@@ -3,6 +3,10 @@
 //
 
 #include "com_example_oppo_demo_JNIFunction.h"
+#include "net.h"
+
+ncnn::Mat in;
+ncnn::Mat out;
 
 JNIEXPORT jstring JNICALL Java_com_example_oppo_1demo_JNIFunction_test
   (JNIEnv *env, jobject obj, jstring name) {
